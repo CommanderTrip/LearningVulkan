50ef76bf63d416ab4a62be05a8bfe27db38fe6ec
Jason <dev@jasoncarnahan.com>
2025-08-27 23:41:18 -0500

    Init the window

--- main.cpp
+++ main.cpp
@@ -1,43 +1,48 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
+#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
-#include <cstdlib>
 
-class HelloTriangleApplication
-{
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+    GLFWwindow *window;
+
 public:
-    void run()
-    {
+    void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
-    void initVulkan()
-    {
-    }
+    void initWindow() {
+        glfwInit();  // Initialize the GLFW Library
 
-    void mainLoop()
-    {
-    }
+        // Do not create an OpenGL context
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
-    void cleanup()
-    {
+        // Disable resizing for now since it needs special care
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        // Create the actual window
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
+    void initVulkan() {}
+    void mainLoop() {}
+    void cleanup() {}
 };
 
-int main()
-{
+int main() {
     HelloTriangleApplication app;
 
-    try
-    {
+    try {
         app.run();
-    }
-    catch (const std::exception& e)
-    {
+    } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
