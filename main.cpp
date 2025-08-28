6863fe7d7b0ce03e0d66ec7242d691609fd15b35
Jason <dev@jasoncarnahan.com>
2025-08-28 14:22:22 -0500

    Create the Vk Instance

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
     GLFWwindow *window;
+    VkInstance instance;
 
 public:
     void run() {
@@ -32,8 +33,41 @@ private:
         // Create the actual window
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
+    void createInstance() {
+        // This is the connection between my app and the Vulkan lib
 
-    void initVulkan() {}
+        // Info about our application
+        // It's optional but helps the driver optimize
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Vk First Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        // Not optional and includes information on what Vulkan extensions and validation layers to include
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        // GLFW will handle windows for us so we need to get the extensions it needs and pass it to Vulkan
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        // Leaving validation layers empty for now
+        createInfo.enabledLayerCount = 0;
+
+        // That's everything specified. Create the Vulkan Instance. If everything goes well, then all the information is
+        // stored in the instance handle. vkCreateInstance will either return VK_SUCCESS or an error for us to check
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create Vk Instance");
+        }
+    }
+
+    void initVulkan() { createInstance(); }
 
     void mainLoop() {
         while (!glfwWindowShouldClose(window)) {
